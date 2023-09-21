bfdc8921e6ddd362de705935bbf40909515b842a
ajay <chuhan718@gmail.com>
2023-09-21 11:05:37 +0530

    convert require to import changes

--- backend/datsbase.ts
+++ backend/datsbase.ts
@@ -1,5 +1,5 @@
 
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 const Url = 'mongodb+srv://school:student123@cluster0.64833pk.mongodb.net/student?retryWrites=true&w=majority';
 mongoose.connect(Url);
 
